89eb6485ee1ebbda75928d8fb08090da28afa6ab
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2023-12-20 14:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const parseCode = (str) => {
-  // your code here
-};
+// // const regEx= /very/g;
 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const text="Harry is very very nice very awesome very nice lorem";
+
+// console.log(text.replace("Harry","Lovely"));
+
+const parseCode = (str) => {
+    // your code here
+    let regEx=/0+/g; //best use of Regular Expression bro
+    let[a,b,c]=str.split(regEx);
+
+    console.log(a,b,c);
+    return {
+        firstName:a,
+        lastName:b,
+        id:c
+    
+  };
+
+}
+
+  
+  // Do not change the code below
+  const str = prompt("Enter str: ");
+  alert(JSON.stringify(parseCode(str)));
